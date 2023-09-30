d07b51f03d8bb8352eb8351ed9e0788fcc44cc31
Грищук Александр <grishchuk@sfedu.ru>
2023-09-30 11:23:34 +0300

    Add year checker

---
+++ time.pas
@@ -0,0 +1,10 @@
+﻿begin
+  var year := ReadInteger('Введите год:');
+  if year mod 4 = 0 then
+  begin
+    if (year mod 100 = 0) and (year mod 400 <> 0) then
+      print('Не високосный год')
+    else print('Високосный год');
+  end
+  else print('Не високосный год');
+end.
